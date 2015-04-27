71320fdb93c462728e0a02a710662bcda8826cb2
Rebecca Schlussel <rebecca.schlussel@teradata.com>
2015-04-27 10:06:17 -0400

    Add maven build infrastructure for product-tests
    Summary:
    Product tests for presto-admin will be written in java.  This adds
    the infrastructure to build it with maven and pull in the test-framework jar
    
    Test Plan: mvn clean install
    
    Reviewers: cwallin, anu
    
    Reviewed By: cwallin, anu
    
    Subscribers: mf186042
    
    Differential Revision: https://phabricator.td.teradata.com/D369

---
+++ presto-admin-test/src/main/java/com/teradata/presto-admin/App.java
@@ -0,0 +1,13 @@
+package com.teradata.prestoadmin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
