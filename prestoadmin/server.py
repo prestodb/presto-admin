d26dc9952bb9125ee837dc7fda063498a5e06d1a
Lukasz Osipiuk <lukasz.osipiuk@teradata.com>
2015-05-27 10:59:44 +0200

    Update license header in source files
    Test Plan: None
    
    Reviewers: wbiela
    
    Subscribers: cwallin
    
    Differential Revision: https://phabricator.td.teradata.com/D650

--- prestoadmin/server.py
+++ prestoadmin/server.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 """
 Module for installing, monitoring and controlling presto server
 using presto-admin
