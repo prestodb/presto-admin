64b6cade9f340a05ab4774c764d08cd1e76dd656
Rebecca Schlussel <rebecca.schlussel@teradata.com>
2015-07-22 13:47:18 -0400

    Add command to run script on cluster
    Add a command to run a script on all nodes in a cluster
    syntax: presto-admin script run <path-to-script> [<remote-dir>]
    
    Testing: make lint test.  The test_script product tests

---
+++ prestoadmin/script.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Commands for running scripts on a cluster
+"""
+import logging
+from fabric.operations import put, sudo
+from fabric.decorators import task
+from os import path
+
+_LOGGER = logging.getLogger(__name__)
+__all__ = ['run']
+
+
+@task
+def run(script, remote_dir='/tmp'):
+    """
+    Run an arbitrary script on all nodes in the cluster.
+
+    Parameters:
+        script - The path to the script
+        remote_dir - Where to put the script on the cluster.  Default is /tmp.
+    """
+    script_name = path.basename(script)
+    remote_path = path.join(remote_dir, script_name)
+    put(script, remote_path)
+    sudo('chmod u+x %s' % remote_path)
+    sudo(remote_path)
+    sudo('rm %s' % remote_path)
---
+++ tests/unit/test_script.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Tests the script module
+"""
+
+from mock import patch, call
+from prestoadmin import script
+
+from tests.base_test_case import BaseTestCase
+
+
+class TestInstall(BaseTestCase):
+
+    @patch('prestoadmin.script.sudo')
+    @patch('prestoadmin.script.put')
+    def test_script_basic(self, put_mock, sudo_mock):
+        script.run('/my/local/path/script.sh')
+        put_mock.assert_called_with('/my/local/path/script.sh',
+                                    '/tmp/script.sh')
+        sudo_mock.assert_has_calls(
+            [call('chmod u+x /tmp/script.sh'), call('/tmp/script.sh'),
+             call('rm /tmp/script.sh')], any_order=False)
+
+    @patch('prestoadmin.script.sudo')
+    @patch('prestoadmin.script.put')
+    def test_script_specify_dir(self, put_mock, sudo_mock):
+        script.run('/my/local/path/script.sh', '/my/remote/path')
+        put_mock.assert_called_with('/my/local/path/script.sh',
+                                    '/my/remote/path/script.sh')
+        sudo_mock.assert_has_calls(
+            [call('chmod u+x /my/remote/path/script.sh'),
+             call('/my/remote/path/script.sh'),
+             call('rm /my/remote/path/script.sh')], any_order=False)
