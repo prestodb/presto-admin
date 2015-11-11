d0952d0136da43b68111b1272645e8d0a4ef671a
Eric Diven <eric.diven@teradata.com>
2015-11-11 14:31:48 -0500

    Basic tests work. Hooray.

---
+++ tests/product/yarn_slider/slider_presto_installer.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License
+"""
+Installer for presto using slider
+"""
+import fnmatch
+import os
+
+import prestoadmin
+from prestoadmin.slider.config import APPNAME, SLIDER_USER, HOST
+
+from tests.base_installer import BaseInstaller
+from tests.product.prestoadmin_installer import PrestoadminInstaller
+from tests.product.yarn_slider.slider_installer import SliderInstaller
+
+PRESTO_YARN_PACKAGE_GLOB = 'presto-yarn-package*.zip'
+
+
+class SliderPrestoInstaller(BaseInstaller):
+    def __init__(self, testcase):
+        (self.package_dir, self.package_filename) = \
+            self._detect_presto_package(testcase)
+        self.testcase = testcase
+
+    @staticmethod
+    def get_dependencies():
+        return [PrestoadminInstaller, SliderInstaller]
+
+    @staticmethod
+    def _get_slider_master(testcase):
+        return testcase.default_keywords[HOST]
+
+    @staticmethod
+    def _get_slider_user(testcase):
+        return testcase.default_keywords[SLIDER_USER]
+
+    def install(self):
+        cluster = self.testcase.cluster
+
+        package_name = self._copy_package_to_pa_host()
+
+        cmd_output = self.testcase.run_prestoadmin(
+            ' slider install ' +
+            os.path.join(cluster.mount_dir, package_name),
+            cluster=cluster)
+
+        return cmd_output
+
+    def get_keywords(self, *args, **kwargs):
+        return {}
+
+    @staticmethod
+    def _detect_presto_package(testcase):
+        search_dir = prestoadmin.main_dir
+        package_names = fnmatch.filter(os.listdir(search_dir),
+                                       PRESTO_YARN_PACKAGE_GLOB)
+
+        testcase.assertNotEqual(0, len(package_names),
+                                'No presto-yarn package found in %s' %
+                                (search_dir))
+        testcase.assertEqual(1, len(package_names),
+                             'Multiple presto-yarn packages found in %s:\n%s'
+                             % (search_dir, '\n'.join(package_names)))
+        return search_dir, package_names[0]
+
+    @staticmethod
+    def assert_installed(testcase, package_filename=None):
+        if package_filename is None:
+            _, package_filename = \
+                SliderPrestoInstaller._detect_presto_package(testcase)
+
+        si = SliderInstaller(testcase)
+        keywords = si.get_keywords()
+        hdfs_cmd = 'hdfs dfs -ls %s' % (
+            os.path.join('/', 'user', keywords[SLIDER_USER], '.slider',
+            'package', keywords[APPNAME],
+            os.path.basename(package_filename))
+        )
+        testcase.cluster.exec_cmd_on_host(
+            SliderPrestoInstaller._get_slider_master(testcase), hdfs_cmd,
+            SliderPrestoInstaller._get_slider_user(testcase))
+
+    def _copy_package_to_pa_host(self):
+        package_path = os.path.join(self.package_dir, self.package_filename)
+
+        self.testcase.cluster.copy_to_host(
+            package_path, self.testcase.cluster.master)
+
+        return self.package_filename
---
+++ tests/product/yarn_slider/test_server_install.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License
+"""
+Tests for the server install sub command.
+"""
+
+from errno import ECONNREFUSED
+from socket import socket
+import time
+
+from tests.product.base_product_case import BaseProductTestCase
+from tests.hdp_bare_image_provider import HdpBareImageProvider
+from tests.product.yarn_slider.slider_presto_installer import \
+    SliderPrestoInstaller
+
+HDFS_PORT = 8020
+
+
+class TestServerInstall(BaseProductTestCase):
+    def setUp(self):
+        super(TestServerInstall, self).setUp()
+        self.setup_cluster(HdpBareImageProvider(), self.PA_SLIDER_CLUSTER)
+        self.installer = SliderPrestoInstaller(self)
+        self.await_hdfs()
+
+    def await_hdfs(self):
+        start = time.clock()
+        ip_addr = self.cluster.get_ip_address_dict()[self.cluster.master]
+        while True:
+            try:
+                s = socket()
+                s.connect((ip_addr, HDFS_PORT))
+                break
+            except Exception as e:
+                s.close()
+                if e.errno == ECONNREFUSED:
+                    pass
+                else:
+                    raise
+            finally:
+                s.close()
+        end = time.clock()
+        duration = end - start
+        print 'Waited %.3f seconds for hdfs' % (duration)
+
+
+    def assert_uninstalled(self):
+        self.assertRaisesRegexp(OSError, 'No such file or directory',
+                                self.installer.assert_installed, self)
+
+    def test_install(self):
+        self.assert_uninstalled()
+        self.installer.install()
+        self.installer.assert_installed(self)
+
+    def test_install_twice(self):
+        self.assert_uninstalled()
+        self.installer.install()
+        self.installer.assert_installed(self)
+        self.assertRaisesRegexp(OSError, "Package exists",
+                                self.installer.install)
+
+    def test_uninstall(self):
+        self.assert_uninstalled()
+        self.installer.install()
+        self.installer.assert_installed(self)
+        self.uninstall()
+        self.assert_uninstalled()
+
+    def test_uninstall_not_installed(self):
+        self.assert_uninstalled()
+        self.assertRaisesRegexp(OSError, "Package does not exist",
+                                self.uninstall)
+
+
+    def uninstall(self):
+        return self.run_prestoadmin(' slider uninstall')
+
