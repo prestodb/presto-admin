66e580d0e1a6f771d6c7f0565d659915e53a5b14
Eric Diven <eric.diven@teradata.com>
2016-01-26 16:58:38 -0500

    Include pa log in test_status assertion failure messages.
    test_status relies on comparing the actual output of presto-admin server status
    to the expected output. Unfortunately, the status command is written in such a
    way that some failures in PrestoClient are logged and otherwise silently
    ignored. This results in incomplete output and an assertion failure with no
    information about what the failure actually was. Since this is intermittent
    and usually happens on Jenkins jobs, we don't get a chance to look at the logs
    manually. The solution is to include them in the assertion failure message.

---
+++ tests/unit/test_base_test_case.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Module for validating functionality in BaseTestCase.
+"""
+
+from base_unit_case import BaseUnitCase
+
+
+class TestBaseTestCase(BaseUnitCase):
+    def testLazyPass(self):
+        self.assertLazyMessage(
+            self.assertEqual, 1, 1, lambda: self.fail("shouldn't be called"))
+
+    def testLazyFail(self):
+        a = 2
+        e = 1
+
+        self.assertRaisesRegexp(
+            AssertionError, 'asdfasdfasdf 2 1', self.assertLazyMessage,
+            self.assertEqual, a, e, lambda: 'asdfasdfasdf %d %d' % (a, e))
