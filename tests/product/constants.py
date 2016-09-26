c7247a4d81bf767ae2f71944fbf87375401e5f85
Eric Diven <eric.diven@teradata.com>
2016-09-26 14:56:15 -0400

    Move base images tag info into its own file
    This makes it possible to run tests through pycharm or nosetests
    without fiddling with environment variables.

--- tests/product/constants.py
+++ tests/product/constants.py
@@ -16,16 +16,25 @@
 Module defining constants global to the product tests
 """
 
+import json
 import os
 import sys
 
 import prestoadmin
 from prestoadmin import main_dir
 
+BASE_IMAGES_TAG_CONFIG = 'base-images-tag.json'
+
+#
+# See the Makefile for an in-depth explanation of how we're using the base
+# Docker images.
+#
 try:
-    BASE_IMAGES_TAG = os.environ['BASE_IMAGES_TAG']
+    with open(os.path.join(main_dir, BASE_IMAGES_TAG_CONFIG)) as tag_config:
+        tag_json = json.load(tag_config)
+    BASE_IMAGES_TAG = tag_json['base_images_tag']
 except KeyError:
-    print "BASE_IMAGES_TAG must be set in the environment"
+    print "base_images_tag must be set in %s" % (BASE_IMAGES_TAG_CONFIG,)
     sys.exit(1)
 
 
