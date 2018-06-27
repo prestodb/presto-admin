72edb28df961b2ac17af12bdfed8a9a7f5e2de87
Karol Sobczak <karol.sobczak@karolsobczak.com>
2018-06-27 16:47:33 +0200

    Add explicit Fabric dependencies

--- setup.py
+++ setup.py
@@ -44,6 +44,8 @@ with open('README.md') as readme_file:
 requirements = [
     'argparse==1.4',
     'paramiko==1.15.3',
+    'cffi==1.4.1',
+    'PyNaCl==1.2.1',
     'Fabric==1.10.1',
     'requests==2.7.0',
     'overrides==0.5',
