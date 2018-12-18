a086a8fd81faa92a33f1dcea4869ccdff63b4c16
Anu Sudarsan <anuatinfy@gmail.com>
2018-12-18 12:39:43 -0500

    Pin idna version

--- tests/product/prestoadmin_installer.py
+++ tests/product/prestoadmin_installer.py
@@ -115,7 +115,7 @@ class PrestoadminInstaller(BaseInstaller):
                 # use explicit versions of dependent packages
                 'pip install --upgrade pycparser==2.18 cffi==1.11.5\n'
                 'pip install --upgrade pycparser==2.18 PyNaCl==1.2.1\n'
-                'pip install --upgrade pycparser==2.18 cryptography==2.1.1\n'
+                'pip install --upgrade pycparser==2.18 idna==2.1 cryptography==2.1.1\n'
                 'pip install --upgrade pip==7.1.2\n'
                 'pip install --upgrade wheel==0.23.0\n'
                 'pip install --upgrade setuptools==20.1.1\n'
